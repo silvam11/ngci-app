364955ecd6fea70655663f044315088eef6200a7
russed5 <David.R@Dell.com>
2018-06-14 14:19:30 +0100

    Update CI pipeline files

---
+++ ci_tasks/task_build_executable.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+set -x
+
+export GOPATH=$(pwd)
+#/gopath:$(pwd)/gopath/src/github.com/cloudfoundry-community/simple-go-web-app/Godeps/_workspace
+#cd gopath/src/github.com/cloudfoundry-community/simple-go-web-app/
+ls -la
+cd skeleton/$SERVICE_NAME
+ls -la
+CGO_ENABLED=0 GOOS=linux go build -a -ldflags '-extldflags "-static"' $SERVICE_NAME.go
+ls -laR ..
+cp $SERVICE_NAME ../binaries
+# cp version.txt ../binaries
+cp Dockerfile ../binaries
